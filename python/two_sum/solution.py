9a9ead268b54d32b956c2705741141ff7f602a44
Ranjani Krishnan <ranjanikrishnanr@gmail.com>
2020-06-29 11:15:50 +0200

    added solution to two_sum

---
+++ python/two_sum/solution.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+    sum_indexes = {}
+    for index, value in enumerate(nums):
+        if (target - value) in sum_indexes:
+            return index, sum_indexes[target-value]
+        sum_indexes[value] = index
+
+
+two_sum_indexes = twoSum([2, 7, 11, 15], 9)
+print(two_sum_indexes)
