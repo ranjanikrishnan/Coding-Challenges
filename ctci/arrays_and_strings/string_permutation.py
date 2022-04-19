9ca88cc2c4183e170757a7a0601a26fd895a27f7
Ranjani <ranjanikrishnanr@gmail.com>
2022-04-19 23:06:36 +0200

    add alternative approach to solving string_permutation, add test

--- ctci/arrays_and_strings/string_permutation.py
+++ ctci/arrays_and_strings/string_permutation.py
@@ -1,9 +1,36 @@
 # given two strings, write a method to determine if one is a permutation of the other
 
-def string_permutation(string_1, string_2):
+def string_permutation_one(string_1, string_2):
     sorted_string_1 = sorted(string_1)
     sorted_string_2 = sorted(string_2)
     if sorted_string_1 == sorted_string_2:
         return True
     return False
+
+
+def string_permutation_two(string_1, string_2):
+    string_1_characters = {}
+    string_1_count = 1
+    string_2_count = 1
+    for character in string_1:
+        print(character)
+        if character in string_1_characters:
+            string_1_characters[character] = [string_1_count+1]
+        else:
+            string_1_characters[character] = [string_1_count]
+
+    for character in string_2:
+        if character in string_1_characters:
+            string_1_characters[character].append(string_2_count)
+        else:
+            return False
+
+    for value in string_1_characters.values():
+        print(value)
+        if len(value) != 2:
+            return False
+        elif value[0] != value[1]:
+            return False
+        return True
+
 
--- ctci/arrays_and_strings/test_string_permutation.py
+++ ctci/arrays_and_strings/test_string_permutation.py
@@ -1,18 +1,34 @@
 import unittest
-from string_permutation import string_permutation
+from string_permutation import string_permutation_one, string_permutation_two
 
 
 class TestDuplicateCharacters(unittest.TestCase):
     def test_string_permutation(self):
-        result = string_permutation("kut", "tuk")
+        result = string_permutation_one("kut", "tuk")
         self.assertEqual(result, True)
 
-        result = string_permutation("abcd", "dabc")
+        result = string_permutation_one("abcd", "dabc")
         self.assertEqual(result, True)
 
-        result = string_permutation("abd", "dabc")
+        result = string_permutation_one("abd", "dabc")
         self.assertEqual(result, False)
 
-        result = string_permutation("cad", "gdc")
+        result = string_permutation_one("cad", "gdc")
         self.assertEqual(result, False)
+
+    def test_string_permutation_two(self):
+        result = string_permutation_two("aacd", "dabc")
+        self.assertEqual(result, False)
+
+        result = string_permutation_two("cad", "gdc")
+        self.assertEqual(result, False)
+
+        result = string_permutation_two("abd", "dabc")
+        self.assertEqual(result, False)
+
+        result = string_permutation_two("abcd", "dabc")
+        self.assertEqual(result, True)
+
+        result = string_permutation_two("kut", "tuk")
+        self.assertEqual(result, True)
 
