ae9dc40f43e3a0c51f89da724e523ac7e8163a16
Ranjani <ranjanikrishnanr@gmail.com>
2022-05-02 22:06:36 +0200

    add solution for matrix rotation in place

--- ctci/arrays_and_strings/matrix_rotation/matrix_rotation.py
+++ ctci/arrays_and_strings/matrix_rotation/matrix_rotation.py
@@ -28,3 +28,23 @@ def copy_matrix(matrix: List[List[int]]) -> List[List[int]]:
             matrix_copy_inner[j] = matrix[i][j]
         copied_matrix[i] = matrix_copy_inner
     return copied_matrix
+
+
+def transpose_matrix(matrix: List[List[int]]):
+    for i in range(len(matrix)):
+        for j in range(i+1, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    return matrix
+
+
+def reverse_rows_in_matrix(matrix: List[List[int]]):
+    for i in range(len(matrix)):
+        # matrix[i] = matrix[i][::-1]
+        matrix[i].reverse()
+    return matrix
+
+
+def matrix_rotation_in_place(matrix: List[List[int]]):
+    transpose_matrix(matrix)
+    reverse_rows_in_matrix(matrix)
+    return matrix
--- ctci/arrays_and_strings/matrix_rotation/test_matrix_rotation.py
+++ ctci/arrays_and_strings/matrix_rotation/test_matrix_rotation.py
@@ -1,5 +1,6 @@
 import unittest
-from ctci.arrays_and_strings.matrix_rotation.matrix_rotation import print_matrix, matrix_rotation, copy_matrix
+from ctci.arrays_and_strings.matrix_rotation.matrix_rotation import print_matrix, matrix_rotation, \
+     copy_matrix, transpose_matrix, reverse_rows_in_matrix, matrix_rotation_in_place
 
 
 class TestMatrixRotation(unittest.TestCase):
@@ -27,3 +28,34 @@ class TestMatrixRotation(unittest.TestCase):
         copied_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         result = copy_matrix(input_matrix)
         self.assertEqual(result, copied_matrix)
+
+    def test_transpose_matrix(self):
+        input_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        output_matrix = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+        matrix = transpose_matrix(input_matrix)
+        self.assertEqual(matrix, output_matrix)
+
+    def test_reverse_rows(self):
+        input_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        output_matrix = [[3, 2, 1], [6, 5, 4], [9, 8, 7]]
+        matrix = reverse_rows_in_matrix(input_matrix)
+        self.assertEqual(matrix, output_matrix)
+
+    def test_matrix_rotation_in_place(self):
+        input_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        print('Input matrix: \n')
+        print_matrix(input_matrix)
+        output_matrix = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+        result = matrix_rotation_in_place(input_matrix)
+        print('Output matrix: \n')
+        print_matrix(result)
+        self.assertEqual(result, output_matrix)
+
+        input_matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+        print('Input matrix: \n')
+        print_matrix(input_matrix)
+        output_matrix = [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+        result = matrix_rotation_in_place(input_matrix)
+        print('Output matrix: \n')
+        print_matrix(result)
+        self.assertEqual(result, output_matrix)
